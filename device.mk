1028d00bef5f97e1aa4c253cac09ace9efe03038
HDzungx <hdzungx@gmail.com>
2026-07-17 10:39:25 +0700

    munch: Migrate TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED to soong
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- device.mk
+++ device.mk
@@ -146,6 +146,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
+$(call soong_config_set_bool,camera,override_format_from_reserved,true)
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
