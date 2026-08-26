df9f709591701e08aadadf740102b3fa73321a7e
HariGaneshM <mallahariganesh98@gmail.com>
2026-08-26 12:27:11 +0000

    PixelOS: Update configs for PixelOS

--- custom_munch.mk
+++ custom_munch.mk
@@ -4,21 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+# Inherit from those products. Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
+# Inherit some common lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit some common PixelOS stuff
+$(call inherit-product, vendor/custom/config/common.mk)
 
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-PRODUCT_NAME := lineage_munch
+PRODUCT_NAME := custom_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := 22021211RC
+PRODUCT_BRAND := Poco
+PRODUCT_MODEL := 22021211RG
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
